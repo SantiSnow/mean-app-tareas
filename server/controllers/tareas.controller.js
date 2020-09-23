121c7509cb47e1e8e5bc6977aac484dd316a9e63
santiago aguirre perez <aguirresantiago81@gmail.com>
2020-09-23 11:28:34 -0300

    agrego controllers y routes

--- server/controllers/tareas.controller.js
+++ server/controllers/tareas.controller.js
@@ -6,9 +6,27 @@ TareasController.getTareas = (req, res) =>{
     });
 }
 
+TareasController.getTarea = (req, res) => {
+    res.json({
+        solicitud: "Tarea seleccionada"
+    });
+}
+
 TareasController.setTareas = (req, res) => {
     res.json({
         solicitud: "Subiendo tareas de la aplicacion"
+    });
+}
+
+TareasController.actualizarTareas = (req, res) => {
+    res.json({
+        solicitud: "Actualizando tarea de la aplicacion"
+    });
+}
+
+TareasController.deleteTareas = (req, res) => {
+    res.json({
+        solicitud: "Eliminando tareas de la aplicacion"
     });
 }
 
--- server/routes/tareas.routes.js
+++ server/routes/tareas.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const tareasCtrlr = require('../controllers/tareas.controller'); 
 
 router.get('/', tareasCtrlr.getTareas);
-
+router.get('/:id', tareasCtrlr.getTarea);
 router.post('/', tareasCtrlr.setTareas);
+router.put('/:id', tareasCtrlr.actualizarTareas);
+router.delete('/:id', tareasCtrlr.deleteTareas);
 
 module.exports = router;
