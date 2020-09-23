d4a0f927849014fbfd2296abe001171a89c5920d
santiago aguirre perez <aguirresantiago81@gmail.com>
2020-09-23 12:25:32 -0300

    terminado el back end del crud

--- server/controllers/tareas.controller.js
+++ server/controllers/tareas.controller.js
@@ -21,11 +21,26 @@ TareasController.setTareas = async (req, res) => {
 }
 
 TareasController.actualizarTareas = async (req, res) => {
-    
+    const tarea = {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        fechaLimite: req.body.fechaLimite,
+        importancia: req.body.importancia,
+        solicitante: req.body.solicitante,
+        entregada: req.body.entregada
+    }
+    await Tarea.findByIdAndUpdate(req.params.id, { $set: tarea }, { new: true });
+    res.json({ status: "Tarea actualizada"});
 }
 
-TareasController.deleteTareas = (req, res) => {
+TareasController.deleteTareas = async (req, res) => {
     
+    await Tarea.findByIdAndRemove(req.params.id);
+
+    res.json({
+        status: "Tarea eliminada"
+    })
+
 }
 
 module.exports = TareasController;
